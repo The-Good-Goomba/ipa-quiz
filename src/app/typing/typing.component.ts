54bbcd6ac1dd96eab0c9ff853576e6e7b66a774d
The-Good-Goomba <86233119+The-Good-Goomba@users.noreply.github.com>
2022-05-13 11:31:13 +1000

    Added Caret
    Need to fix caret position

--- src/app/typing/typing.component.ts
+++ src/app/typing/typing.component.ts
@@ -1,5 +1,5 @@
 import { HttpErrorResponse } from '@angular/common/http';
-import { Component, OnInit, Input } from '@angular/core';
+import { Component, OnInit, Input, ViewChild, ElementRef } from '@angular/core';
 import { DictionaryService } from '../dictionary/dictionary.service';
 import { ipaStruct } from '../ipaStruct';
 let english_1k = require('./text-files/english_1k.json');
@@ -19,9 +19,11 @@ export class TypingComponent {
   title = 'ipa-quiz';
   typedWord:string = '';
   currentIPA: ipaStruct;
-  allWords: ipaStruct[] = [];
-  indexEnglish: number = 0;
-  indexIPA: number = 0;
+  useFile: boolean = true;
+
+  // allWords: ipaStruct[] = [];
+  // indexEnglish: number = 0;
+  // indexIPA: number = 0;
 
   constructor(private dictionary: DictionaryService) {
     this.currentIPA = {
@@ -29,8 +31,8 @@ export class TypingComponent {
       word:'',
       audio: ''
     }
-    this.currentIPA.word = this.newWord();
-    this.runIpa(this.currentIPA.word);
+    
+    this.runIpa();
   }
 
   
@@ -43,22 +45,10 @@ export class TypingComponent {
       this.typedWord = this.typedWord.concat(event.key);
 
       if (this.typedWord == this.currentIPA.word) {
-        this.currentIPA.word = this.newWord();
+        this.runIpa()
         this.typedWord = '';
-        this.runIpa(this.currentIPA.word);
       } 
     }
-  }
-
-  newWord(): string
-  {
-    var index = Math.floor(Math.random() * english_1k.words.length);
-    let word: string = english_1k.words[index];
-    // if (this.indexEnglish > english_1k.words.length)
-    // {
-    //   this.writeContents(amongus,'ipa_1k.json','text/json');
-    // }
-    return word;
   }
 
   writeContents(content: string, fileName: string, contentType: string) {
@@ -70,15 +60,16 @@ export class TypingComponent {
   }
 
   setStruct(data: any) {
-    // console.log(data);
+
     this.currentIPA.ipa = data[0].phonetic;
     this.currentIPA.word = data[0].word;
     this.currentIPA.audio = data[0].phonetics[0].audio;
 
     if ( this.currentIPA.ipa == null)
     {
-      this.runIpa(this.newWord());
+      this.runIpa();
     }
+    
     // let tempIPA: ipaStruct = {
     //   word: this.currentIPA.word,
     //   audio: this.currentIPA.audio,
@@ -86,24 +77,40 @@ export class TypingComponent {
     // };
     // tempIPA.audio = this.currentIPA.audio;
     // this.allWords[this.indexIPA] = tempIPA;
-
     // this.indexIPA += 1;
+
   }
 
-  runIpa(wordToConvert: string)
+  runIpa()
   {
-    this.dictionary.getData(wordToConvert).subscribe(
-      (data: any) => {
-        this.setStruct(data);
-      },
-      (error: HttpErrorResponse) => {
-        console.log('Cant find this word');
-        this.runIpa(this.newWord());
+
+    if (this.useFile){
+      var index = Math.floor(Math.random() * ipa_1k.hi.length);
+      this.currentIPA = ipa_1k.hi[index];
+  
+      // if (this.indexEnglish > english_1k.words.length)
+      // {
+      //   this.writeContents(amongus,'ipa_1k.json','text/json');
+      // }
+      if (this.currentIPA.ipa == null)
+      {
+        console.log(`No IPI for ${this.currentIPA.word}`)
+      }
+      
+    } else {
+      var index = Math.floor(Math.random() * english_1k.words.length);
+      let word: string = english_1k.words[index];
+
+      this.dictionary.getData(word).subscribe(
+        (data: any) => {
+          this.setStruct(data);
+        },
+        (error: HttpErrorResponse) => {
+          console.log('Cant find this word');
+          this.runIpa();
+      });
     }
-    );
 
   }
-
-
 
 }
