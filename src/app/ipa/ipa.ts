eea52aa35da2bfaf3d933fa5a06e66df122fcb69
The-Good-Goomba <86233119+The-Good-Goomba@users.noreply.github.com>
2022-06-10 15:10:36 +1000

    mc-choice is close

--- src/app/ipa/ipa.ts
+++ src/app/ipa/ipa.ts
@@ -5,105 +5,100 @@ let english_1k = require('./../../assets/english_1k.json');
 let english_450k = require('./../../assets/english_450k.json');
 
 export class Ipa {
-    currentIPA: ipaStruct;
-    nextIPA: ipaStruct;
-    nextWord: boolean = false;
-    english = english_1k;
+	currentIPA: ipaStruct;
+	nextIPA: ipaStruct;
+	nextWord: boolean = false;
+	english = english_1k;
 
-    constructor(private dictionary: DictionaryService, wordSize: (struct: ipaStruct) => void) {
-        this.currentIPA = {
-            ipa: '',
-            word: '',
-            audio: '',
-        }
-        this.nextIPA = {
-            ipa: '',
-            word: '',
-            audio: '',
-        }
+	constructor(private dictionary: DictionaryService, wordSize: (struct: ipaStruct) => void) {
+		this.currentIPA = {
+			ipa: '',
+			word: '',
+			audio: '',
+		}
+		this.nextIPA = {
+			ipa: '',
+			word: '',
+			audio: '',
+		}
 
-        this.wordSizeFunc = wordSize;
-        this.runIpa(this.currentIPA);
-        this.runIpa(this.nextIPA);
-    }
+		this.wordSizeFunc = wordSize;
+		this.runIpa(this.currentIPA);
+		this.runIpa(this.nextIPA);
+	}
 
-    wordSizeFunc(struct: ipaStruct) {}
+	wordSizeFunc(struct: ipaStruct) { }
 
-    setCurrentIPA()
-    {
-      this.currentIPA.ipa = this.nextIPA.ipa;
-      this.currentIPA.word = this.nextIPA.word;
-      this.currentIPA.audio = this.nextIPA.audio;
-      this.wordSizeFunc(this.currentIPA);
-    }
+	private setCurrentIPA() {
+		this.currentIPA.ipa = this.nextIPA.ipa;
+		this.currentIPA.word = this.nextIPA.word;
+		this.currentIPA.audio = this.nextIPA.audio;
+		this.wordSizeFunc(this.currentIPA);
+	}
 
-    nextWordIPA()
-    {
-        
-        try {
-          this.setCurrentIPA();
-          this.runIpa(this.nextIPA);
-        } catch(error) {
-          this.nextWord = true;
-          console.log("Waiting for API...", error);
-          this.runIpa(this.nextIPA);
-        }
-    }
+	nextWordIPA() {
 
-    setStruct(data: any,struct: ipaStruct) {
-        try {
-          struct.ipa = data[0].phonetic;
-          struct.word = data[0].word;
-          struct.audio = data[0].phonetics[0].audio;
-        } catch(error)
-        {
-          for(let i = 0;1 < data[0].phonetics.length; i++)
-          {
-            try {
-              struct.ipa = data[0].phonetics[i].text;
-              if (struct.ipa != null)
-              {
-                return;
-              }
-            } catch(error) {
-              
-            }
-          }
-          this.runIpa(struct);
-        }
-        if (this.nextWord)
-        {
-           
-          this.setCurrentIPA();
-          this.runIpa(this.nextIPA);
-          this.nextWord = false;
-        }
+		try {
+			this.setCurrentIPA();
+			this.runIpa(this.nextIPA);
+		} catch (error) {
+			this.nextWord = true;
+			console.log("Waiting for API...", error);
+			this.runIpa(this.nextIPA);
+		}
+	}
 
-        if (struct === this.currentIPA)
-        {
-          this.wordSizeFunc(struct);
-        }
-    
-    }
+	private setStruct(data: any, struct: ipaStruct) {
+		try {
+			struct.ipa = data[0].phonetic;
+			struct.word = data[0].word;
+			struct.audio = data[0].phonetics[0].audio;
+			if (struct.ipa == undefined) { throw "No IPA" }
+		} catch (error) {
+			console.log(error);
+			for (let i = 0; 1 < data[0].phonetics.length; i++) {
+				try {
+					struct.ipa = data[0].phonetics[i].text;
+					if (struct.ipa != null) {
+						return;
+					}
+				} catch (error) {
 
-    runIpa(inputIPA: ipaStruct)
-    {
-      var index = Math.floor(Math.random() * this.english.words.length);
-      let word: string = this.english.words[index];
-  
-      console.log("Looking for " + word)
-  
-      this.dictionary.getData(word).subscribe(
-          (data: any) => {
-            this.setStruct(data, inputIPA);
-            console.log('found word')
-          },
-          (error: HttpErrorResponse) => {
-            console.warn('Cant find this word', error)
-            this.runIpa(inputIPA);
-        });
-  
-  
-    }
+				}
+			}
+			this.runIpa(struct);
+		}
+		if (this.nextWord) {
+
+			this.setCurrentIPA();
+			this.runIpa(this.nextIPA);
+			this.nextWord = false;
+		}
+
+		if (struct === this.currentIPA) {
+			console.log('bruh')
+			this.wordSizeFunc(struct);
+		}
+
+	}
+
+	private runIpa(inputIPA: ipaStruct) {
+		var index = Math.floor(Math.random() * this.english.words.length);
+		let word: string = this.english.words[index];
+
+		console.log("Looking for " + word)
+
+		this.dictionary.getData(word).subscribe(
+			(data: any) => {
+				this.setStruct(data, inputIPA);
+				console.log('found word')
+			},
+			(error: HttpErrorResponse) => {
+				console.warn('Cant find this word', error)
+				this.runIpa(inputIPA);
+			});
+
+
+	}
 
 }
--- src/app/multi-choice/multi-choice.component.ts
+++ src/app/multi-choice/multi-choice.component.ts
@@ -1,27 +1,112 @@
+import { parseTemplate } from '@angular/compiler';
 import { Component, OnInit } from '@angular/core';
 import { DictionaryService } from '../dictionary/dictionary.service';
 import { Ipa } from '../ipa/ipa';
 import { ipaStruct } from '../ipaStruct';
+let mistakes = require('./../../assets/mistakes.json');
 
 @Component({
-  selector: 'app-multi-choice',
-  templateUrl: './multi-choice.component.html',
-  styleUrls: ['./multi-choice.component.scss']
+	selector: 'app-multi-choice',
+	templateUrl: './multi-choice.component.html',
+	styleUrls: ['./multi-choice.component.scss']
 })
-export class MultiChoiceComponent implements OnInit {
-  ipa: Ipa;
-  wordFontSize: string = '5vw';
+export class MultiChoiceComponent {
+	ipa: Ipa;
+	wordFontSize: string = '5vw';
+	boxInfo: string[] = [];
+	correctIndex: number = 0;
+
+	constructor(private dictionary: DictionaryService) {
+		this.ipa = new Ipa(this.dictionary, this.callbackFunc);
+	}
+
+	callbackFunc = (struct: ipaStruct): void => {
+		this.wordFontSize = struct.word.length > 10 ? `${25 / struct.word.length}vw` : '5vw';
+		console.log(struct)
+		let temp = this.parseIpa(struct.ipa)
+		this.boxNamed(temp);
+	}
 
 
-  constructor(private dictionary: DictionaryService) {
-    this.ipa = new Ipa(this.dictionary, this.callbackFunc);
-  }
+	parseIpa = (ipa: string): string[] => {
+		let parsed: string[] = [];
+		let ipaArray: string[] = ipa.split('');
+		console.log(ipaArray);
+	
+		var i = 0;
 
-  ngOnInit(): void {
-  }
+		do {
+			if (ipaArray[i + 1] == 'ː')
+			{
+				if (this.checkLetter(ipaArray[i].concat(':')))
+				{
+					parsed.push(ipaArray[i].concat(':'));
+				}
+				i++;
+			} else if (ipaArray[i + 1] == ' ͡' && ipaArray[i] == 't' && ipaArray[i + 2] == 'ʃ') {
+				parsed.push(ipaArray[i].concat('ʃ'));
+				i += 2;
+			} else {
+				if (this.checkLetter(ipaArray[i].concat(ipaArray[i + 1])))
+				{
+					parsed.push(ipaArray[i].concat(ipaArray[i + 1]));
+					i++;
+				}  else if (this.checkLetter(ipaArray[i]))
+				{
+					parsed.push(ipaArray[i]);
+				}
+			}
+			i++;
+		} while (i < ipaArray.length);
+		console.log(parsed);
+		return parsed;
+	}
 
-  callbackFunc = (struct: ipaStruct): void => {
-    this.wordFontSize = struct.word.length > 10 ? `${25/struct.word.length}vw` : '5vw';
-  }
+	randNumbers = (len: number, quantity: number): number[] => {
+
+		let array: number[] = Array.from(Array(len).keys())
+
+		var shuffled = array.sort(function(){return .5 - Math.random()});
+
+		var selected = shuffled.slice(0,quantity);
+		return selected;
+	}
+
+	boxNamed = (bruh: string[]) => {
+		let retWord: string = bruh.join('');
+		let indices: number[] = this.randNumbers(bruh.length, 3);
+		this.correctIndex = Math.floor(Math.random() * 3);
+		for (var i = 0; i < 4; i++) {
+
+			if (i == this.correctIndex) {
+				this.boxInfo[i] = '/' + retWord + '/';
+			} else {
+				let sus: number = ((i > this.correctIndex) ? i - 1 : i);
+				let among: string = bruh[indices[sus]];
+				let amongIndex: number = Math.floor(Math.random() * (mistakes[among].length - 1));
+				let replacement = mistakes[among][amongIndex];
+				console.log(indices[sus] + ' ' + among + ' ' + replacement);
+				this.boxInfo[i] = '/' + retWord.substring(0, indices[sus]) + replacement + retWord.substring(indices[sus] + 2) + '/';
+			}
+		}
+	}
+
+	checkLetter = (letter: string): boolean => {
+		let bruh: string[] = mistakes[letter];
+		if (bruh == undefined){ return false; }
+		else { return true; }
+	}
+
+	guess = (index: number): void => {
+		if (index == this.correctIndex) {
+			console.log('correct');
+			this.ipa.nextWordIPA();
+		} else {
+			console.log('wrong');
+		}
+	}
 
 }
+
+// An array of numbers that point to the index of valid letters
+// And array of 3 numbers random numbers, that point to the index of the valid letters
